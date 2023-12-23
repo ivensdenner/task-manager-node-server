618f55bff39eed92a4e8cc4173690b54971f539b
Ivens Denner <ivensc@superplayer.company>
2023-12-23 18:27:26 -0300

    Add Task

---
+++ src/task.js
@@ -0,0 +1,34 @@
+import { randomUUID } from 'node:crypto'
+
+const Task = {
+    init: function(title, description) {
+        this.title       = title
+        this.description = description
+
+        this.id           = randomUUID()
+        this.completed_at = null
+        this.created_at   = new Date()
+        this.updated_at   = this.created_at
+
+        return this;
+    },
+
+    update: function(title, description) {
+        this.title       = title
+        this.description = description
+
+        this.updated_at = new Date()
+
+        return this;
+    },
+
+    toggleCompletion: function() {
+        if (this.completed_at) {
+            this.completed_at = null
+        } else {
+            this.completed_at = new Date()
+        }
+
+        return this;
+    },
+}
